fe435092edf8aefc8602173691e0da8b01a48b31
Marty Miller <Martin@Martys-MacBook-Pro.local>
2018-09-05 20:01:45 -0700

    finishing assignment 1

--- CS 635 Advanced Object Oriented/PriorityQueue.py
+++ CS 635 Advanced Object Oriented/PriorityQueue.py
@@ -1,87 +1,121 @@
 
-
+#Author: Martin Miller
+#redID: 817405999
+#Professor: Whitney
+#Class: CS 635 Advanced Object Oriented Design
+#Description: The class below inputs students into a priority queue.
+#file: PriorityQueue.py
+#Date: September 5th, 2018
 
 
 class PriorityQueue:
 
+    # Since this is a priority queue, only a "master list" is required to keep track of the students.
+    # The list is defined as empty.
     def __init__(self):
-
         self.master_list = []
-        self.insertion_number = 0
         return
 
+    # The add_to_list function is used to add students, in a ranked order, to the priority queue.
+    # The input parameters include the students information, however this info is passed to the
+    # 'Student' structure before its added to the list.
     def add_to_list(self, name, redID, email, address, GPA, numberOfUnitsTaken):
+
+        # The GPA and number of units is checked before adding it to the list. The design decision was
+        # made to raise a value error, because this can be checked for in our Unit test.
         if GPA > 4.0 or GPA < 0.0:
             raise ValueError("The GPA is too high or too low!")
         elif numberOfUnitsTaken > 150 or numberOfUnitsTaken < 0.0:
             raise ValueError("The number of units taken is too high or too low!")
 
-
+        # The function calls a binary search function to find where in the master list the student
+        # should be added. This value is stored as the 'insertion number'.
         self.insertion_number = self.binary_search(0,self.list_length(),GPA,numberOfUnitsTaken)
+
+        # The student object is created and added to the master list based on the insertion number
+        # that was returned from the binary search.
         self.master_list.insert(self.insertion_number,Students(name, redID, email, address, GPA, numberOfUnitsTaken))
         return self.master_list
 
+    # The rank calculator first normalizes the GPA and number of units, and then computes the rank.
+    # The return value is rounded to 4 decimal places in increase readability for repeating decimals.
     def rank_calculator(self, GPA, numberOfUnitsTaken):
         return round((GPA/4.0)*.3 + (numberOfUnitsTaken/150)*.7,4)
 
+    # The purpose of the binary search is to find where to add the Student object to the master list,
+    # based on its ranking. The design decision was made to use a binary search. This is because
+    # for a binary search, the data is halved with each iteration, by finding the midpoint, and
+    # recursively searching the list greater or less then the midpoint. Therefore it has (log n) complexity.
     def binary_search(self,start,end,GPA,numberOfUnitsTaken):
 
+        # First find the rank of the proposed Student object.
         self.rank = self.rank_calculator(GPA,numberOfUnitsTaken)
-        #print(score)
 
+        # If the master list is currently empty, there is nothing to compare and the Student object
+        # should be added in the zeroth spot.
         if not self.master_list:
             #print("should only happen once")
             return 0
 
+        # This is the case where there is two objects in the master list. Find who has the higher ranking
+        # and return the appropriate value.
         if (start+1) == end:
             if (self.rank_calculator(self.master_list[start].GPA,self.master_list[start].numberOfUnitsTaken)) > self.rank:
-                #print("1 was used")
                 return start
             else:
-                #print("2 was used")
                 return start+1
 
+        # Find the median index of the list, if its not a whole number, minus one half.
         median = (end+start)/2
         if median%1 != 0:
             median = median-.5
         median = int(median)
-        #print("start " + str(start) +" this is median " +str(median) + " and this is end " +str(end))
 
+        # Now for the recursion steps. The following if statement checks if the inserting Student rank is higher
+        # or lower then the midpoint. If it is, recursively call the binary search on the upper (or lower)
+        # half of the list. This process continues until the list is 1 or 2 objects long.
         if (self.rank_calculator(self.master_list[median].GPA,self.master_list[median].numberOfUnitsTaken)) < self.rank:
-            #print("recurse 1 was taken")
             self.rank = self.binary_search(median,end,GPA,numberOfUnitsTaken)  
             return self.rank
         elif (self.rank_calculator(self.master_list[median].GPA,self.master_list[median].numberOfUnitsTaken)) > self.rank:
-            #print("recurse 2 was taken")
             self.rank = self.binary_search(start,median,GPA,numberOfUnitsTaken)
             return self.rank
         else:
-            #print("this was used")
             return median
 
-
+    # The show_name function simple prints the names.
     def show_name(self):
-        #print(self.master_list)
         for x in self.master_list:
             print(x.name)
 
+    # The show_name_redID function prints the names and redIDs of all the Student objects in master list.
     def show_name_redID(self):
         for x in self.master_list:
+            # The .ljust(20) method is used for formatting purposes to make the data more readable.
             print("Name: " + str(x.name.ljust(20)) + "\t\tRedID: " + str(x.redID))
 
-    def pop(self):
-        self.master_list.pop()
+    # The function pop_from_queue removes the element with the highest priority. It does not return it.
+    def pop_from_queue(self):
+        if self.master_list:
+            self.master_list.pop()
         return
 
+    # Simple function to find the length of the master list.
     def list_length(self):
-        #print(len(self.master_list))
         return len(self.master_list)
 
+    # The highest_priority returns the element with the highest priority. It doesnt not remove it from
+    # the master list.
     def highest_priority(self):
-        return self.master_list[-1]
+        if self.master_list:
+            return self.master_list[-1]
+        else:
+            return 1
 
 class Students:
 
+    # The student structure is used as elements in the master list above it.
+    # It contains all the parameters for each student, including their 'ranking' score.
     def __init__(self, name, redID, email, address, GPA, numberOfUnitsTaken):
         self.name = name
         self.redID = redID
@@ -91,12 +125,12 @@ class Students:
         self.numberOfUnitsTaken = numberOfUnitsTaken
         self.rank = 0
 
-List = PriorityQueue()
-List.add_to_list("Delta",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
-List.add_to_list("Echo",123456789, "email@sdsu.edu","123 Fake St",1.2,50.0) 
-List.add_to_list("Indigo",123456789, "email@sdsu.edu","123 Fake St",3.2,80.0)
-List.add_to_list("Helo",123456789, "email@sdsu.edu","123 Fake St",3.0,70.0)
-List.add_to_list("SuperAlpha",123456789, "email@sdsu.edu","123 Fake St",0.8,00.0)
+#List = PriorityQueue()
+##List.add_to_list("Delta",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
+#List.add_to_list("Echo",123456789, "email@sdsu.edu","123 Fake St",1.2,50.0) 
+#List.add_to_list("Indigo",123456789, "email@sdsu.edu","123 Fake St",3.2,80.0)
+#List.add_to_list("Helo",123456789, "email@sdsu.edu","123 Fake St",3.0,70.0)
+#List.add_to_list("SuperAlpha",123456789, "email@sdsu.edu","123 Fake St",0.8,00.0)
 #List.add_to_list("Gamma",123456789, "email@sdsu.edu","123 Fake St",3.8,50.0)
 #List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
 #List.add_to_list("Bravo",123456789, "email@sdsu.edu","123 Fake St",1.8,20.0)
@@ -138,8 +172,7 @@ List.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",4.0,150.0)
 #List.pop()
 ##List.pop()
 ##List.pop()
-List.show_name_redID()
+#List.show_name_redID()
 #print(List.highest_priority())
 
 
-print("------------- done -------------")
--- CS 635 Advanced Object Oriented/test_PriorityQueue.py
+++ CS 635 Advanced Object Oriented/test_PriorityQueue.py
@@ -1,71 +1,93 @@
+#Author: Martin Miller
+#redID: 817405999
+#Professor: Whitney
+#Class: CS 635 Advanced Object Oriented Design
+#Description: The class below inputs students into a priority queue.
+#file: test_PriorityQueue.py
+#Testing file: PriorityQueue.py
+#Date: September 5th, 2018
+
 import unittest
 import PriorityQueue
 
 class TestPriorityQueue(unittest.TestCase):
 
+    # setUp is run before every test. It defines a set of input data that can be tested.
     def setUp(self):
-        self.List = PriorityQueue.PriorityQueue()
-        self.List.add_to_list("Diego",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
-        self.List.add_to_list("Eric",123456789, "email@sdsu.edu","123 Fake St",1.2,50.0)
-        self.List.add_to_list("Indie",123456789, "email@sdsu.edu","123 Fake St",3.2,80.0)
-        self.List.add_to_list("Hank",123456789, "email@sdsu.edu","123 Fake St",3.0,70.0)
-        self.List.add_to_list("Aaron",123456789, "email@sdsu.edu","123 Fake St",0.8,00.0)
-        self.List.add_to_list("Gary",123456789, "email@sdsu.edu","123 Fake St",3.8,50.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Brittany",123456789, "email@sdsu.edu","123 Fake St",1.8,20.0)
-        self.List.add_to_list("Geoff",123456789, "email@sdsu.edu","123 Fake St",3.1,60.0)
-        self.List.add_to_list("Issac",123456789, "email@sdsu.edu","123 Fake St",3.1,150.0)
-        self.List.add_to_list("Frank",123456789, "email@sdsu.edu","123 Fake St",3.6,50.0)
-        self.List.add_to_list("Alex",123456789, "email@sdsu.edu","123 Fake St",1.7,10.0)
-        self.List.add_to_list("Ivan",123456789, "email@sdsu.edu","123 Fake St",2.9,80.0)
-        self.List.add_to_list("Derek",123456789, "email@sdsu.edu","123 Fake St",1.2,40.0)
-        self.List.add_to_list("Doug",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
-        self.List.add_to_list("Jeff",123456789, "email@sdsu.edu","123 Fake St",3.9,150.0)
 
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        # First establish the PriorityQueue class.
+        self.master_list = PriorityQueue.PriorityQueue()
 
-        self.List.add_to_list("Diego",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
-        self.List.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
-        self.List.add_to_list("Brittany",123456789, "email@sdsu.edu","123 Fake St",1.8,20.0)
-        self.List.add_to_list("Alex",123456789, "email@sdsu.edu","123 Fake St",1.7,10.0)
+        # Three different types of data is tested. The data below is random order.
+        # The names of the students are meant to alphabetical in the final priority queue.
+        self.master_list.add_to_list("Diego",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
+        self.master_list.add_to_list("Eric",123456789, "email@sdsu.edu","123 Fake St",1.2,50.0)
+        self.master_list.add_to_list("Indie",123456789, "email@sdsu.edu","123 Fake St",3.2,80.0)
+        self.master_list.add_to_list("Hank",123456789, "email@sdsu.edu","123 Fake St",3.0,70.0)
+        self.master_list.add_to_list("Aaron",123456789, "email@sdsu.edu","123 Fake St",0.8,00.0)
+        self.master_list.add_to_list("Gary",123456789, "email@sdsu.edu","123 Fake St",3.8,50.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Brittany",123456789, "email@sdsu.edu","123 Fake St",1.8,20.0)
+        self.master_list.add_to_list("Geoff",123456789, "email@sdsu.edu","123 Fake St",3.1,60.0)
+        self.master_list.add_to_list("Issac",123456789, "email@sdsu.edu","123 Fake St",3.1,150.0)
+        self.master_list.add_to_list("Frank",123456789, "email@sdsu.edu","123 Fake St",3.6,50.0)
+        self.master_list.add_to_list("Alex",123456789, "email@sdsu.edu","123 Fake St",1.7,10.0)
+        self.master_list.add_to_list("Ivan",123456789, "email@sdsu.edu","123 Fake St",2.9,80.0)
+        self.master_list.add_to_list("Derek",123456789, "email@sdsu.edu","123 Fake St",1.2,40.0)
+        self.master_list.add_to_list("Doug",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
+        self.master_list.add_to_list("Jeff",123456789, "email@sdsu.edu","123 Fake St",3.9,150.0)
+
+        # This set of data tests for repeated values, and to see if they cause issues.
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+
+        # The data below is intentionally backwards order to see if its sorts correctly.
+        self.master_list.add_to_list("Diego",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
+        self.master_list.add_to_list("Charlie",123456789, "email@sdsu.edu","123 Fake St",1.6,30.0)
+        self.master_list.add_to_list("Brittany",123456789, "email@sdsu.edu","123 Fake St",1.8,20.0)
+        self.master_list.add_to_list("Alex",123456789, "email@sdsu.edu","123 Fake St",1.7,10.0)
         
-
+    # tearDown destroys the input data after each test.
     def tearDown(self):
         pass 
 
-
     def test_add_to_list(self):
 
+        # This function checks the ranking and makes sure its in increasing order.
         def compare_list_rank():
             comparison_rank = 0
-            for x in self.List.master_list:
+            for x in self.master_list.master_list:
                 self.assertGreaterEqual(x.rank,comparison_rank)
                 comparison_rank = x.rank
-            
+        
+        # First check to see if the master list is in increaseing order, then pop an element from 
+        # the queue. Then recheck to make sure its in increasing order.
         compare_list_rank()
-        self.List.pop()
+        self.master_list.pop_from_queue()
         compare_list_rank()
 
+        # The tests below check for bad data entry that exceeds the input specifications.
+        # Example being too high a GPA or negative units.
         with self.assertRaises(ValueError):
-            self.List.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",40.0,40.0)
+            self.master_list.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",40.0,40.0)
 
         with self.assertRaises(ValueError):
-            self.List.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",-1.0,40.0)
+            self.master_list.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",-1.0,40.0)
 
         with self.assertRaises(ValueError):
-            self.List.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",4.0,500)
+            self.master_list.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",4.0,500)
 
         with self.assertRaises(ValueError):
-            self.List.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",4.0,-12.9)
+            self.master_list.add_to_list("Jack",123456789, "email@sdsu.edu","123 Fake St",4.0,-12.9)
 
         with self.assertRaises(ValueError):
-            self.List.add_to_list("Jack",123456789,"email@sdsu.edu","123 Fake St",4.0,-12.9)
+            self.master_list.add_to_list("Jack",123456789,"email@sdsu.edu","123 Fake St",4.0,-12.9)
 
+    # The ranking calculator is tested against known values.
     def test_rank_calculator(self):
         result = PriorityQueue.PriorityQueue.rank_calculator(self,2.0,30)
         self.assertEqual(result,.29)
@@ -76,35 +98,66 @@ class TestPriorityQueue(unittest.TestCase):
         result = PriorityQueue.PriorityQueue.rank_calculator(self,0,0)
         self.assertEqual(result,0.0)
 
-
-
+    # The show_name function is tested to see if the types of parameters are whats expected.
+    # This can be tricky in python, as the types are inferred, but may help debug an unseen issue.
     def test_show_name(self):
-        for x in self.List.master_list:
+        for x in self.master_list.master_list:
             self.assertTrue(type(x.name) == type("string"))
 
-        for x in self.List.master_list:
+        for x in self.master_list.master_list:
             self.assertTrue(type(x.redID) == int)
 
-        
-        #for x in self.List.master_list:
-        #    self.List.add_to_list(2,123456789,"email@sdsu.edu","123 Fake St",4.0,10)
-        #    self.assertFalse(type(int(x.name)) == type("string"))
-        ###
-
-    def test_pop(self):
-        lengthlist_before_pop = len(self.List.master_list)
-        self.List.pop()
-        lengthlist_after_pop = len(self.List.master_list)
-        self.assertGreater(lengthlist_before_pop,lengthlist_after_pop)
+    # The test below checks the length of the master list, pops a value from it, then rechecks the 
+    # length of the list. The length of the master list should be one less.
+    def test_pop_from_queue(self):
+        lengthlist_before_pop = len(self.master_list.master_list)
+        self.master_list.pop_from_queue()
+        lengthlist_after_pop = len(self.master_list.master_list)
+        self.assertEqual(lengthlist_before_pop,lengthlist_after_pop+1)
         
 
     def test_highest_priority(self):
 
-        self.List.add_to_list("Zack",123456789, "email@sdsu.edu","123 Fake St",4.0,150.0)
-        self.assertEqual(self.List.highest_priority().name,"Zack")
-        self.assertEqual(self.List.master_list[-1].name,"Zack")
+        # The test below uses a student with the absolute highest ranking. Therefore it checks
+        # to make sure the student is given the highest priority.
+        self.master_list.add_to_list("Zack",123456789, "email@sdsu.edu","123 Fake St",4.0,150.0)
+        self.assertEqual(self.master_list.highest_priority().name,"Zack")
+        self.assertEqual(self.master_list.master_list[-1].name,"Zack")
+
+        # The highest priority method returns a Student element but does not pop it from the master
+        # list. The test below checks the size before and after the method.
+        lengthlist_before = len(self.master_list.master_list)
+        self.master_list.highest_priority()
+        lengthlist_after = len(self.master_list.master_list)
+        self.assertEqual(lengthlist_before,lengthlist_after)
+
+# A seperate class is created to test methods for an empty array.
+class TestPriorityQueue_EmptyQueue(unittest.TestCase):
+
+    def setUp(self):
+        self.master_list = PriorityQueue.PriorityQueue()
+
+    def tearDown(self):
         pass
 
+    # See if you can add a student object to an empty list successfully.
+    def test_add_to_list(self):
+        self.master_list.add_to_list("Diego",123456789, "email@sdsu.edu","123 Fake St",1.0,40.0)
+        self.assertIsNotNone(self.master_list)
 
+    # Since the master list is empty, using the pop method should not change the length list.
+    # Additionally this list checks to see if any errors happen from using the pop method
+    # on an empty list.
+    def test_show_name(self):
+        lengthlist_before_pop = len(self.master_list.master_list)
+        self.master_list.pop_from_queue()
+        lengthlist_after_pop = len(self.master_list.master_list)
+        self.assertEqual(lengthlist_before_pop,lengthlist_after_pop)
+
+    # The highest priority method should return 1 if the master list is empty.
+    def test_highest_priority(self):
+        self.assertEqual(self.master_list.highest_priority(),1)
+
+# Establish and run the test cases.
 if __name__ == '__main__':
     unittest.main()
